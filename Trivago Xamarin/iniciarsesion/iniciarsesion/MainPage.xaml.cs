e5d2931a47d93538ed197148b442ebb238990763
RoNyOsCaRxd <rony.choquecondo@ucsp.edu.pe>
2023-06-26 19:45:35 -0500

    login

---
+++ Trivago Xamarin/iniciarsesion/iniciarsesion/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace login
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void btn_guardar_Clicked(object sender, EventArgs e)
+        {
+            string nombre = x_correo.Text;
+            string apellido = x_contraseña.Text;
+
+
+            
+            DisplayAlert("Iniciar Sesion", "iniciando sesion ;) ", "Aceptar");
+
+
+        }
+
+    }
+}
---
+++ Trivago Xamarin/login/login/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace login
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void btn_guardar_Clicked(object sender, EventArgs e)
+        {
+            string nombre = x_nombre.Text;
+            string apellido = x_apellido.Text;
+            string correo = x_correo.Text;
+            string contraseña = x_contraseña.Text;
+
+            // Aquí puedes realizar las operaciones que desees con los datos ingresados
+
+            // Por ejemplo, mostrar un mensaje de confirmación
+            DisplayAlert("Guardado", "Los datos se han guardado correctamente", "Aceptar");
+            
+            
+        }
+       
+    }
+}
