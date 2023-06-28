46be758c037fcba6cbf036f217e10a60067f5193
rodrigospm <rodrigo.perez@ucsp.edu.pe>
2023-06-28 00:22:30 -0500

    catalogue con fire database

---
+++ Trivago Xamarin/catalogue/Trivago/Trivago/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Trivago.Vista;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Trivago
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new GuardarHotel();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Trivago Xamarin/catalogue/Trivago/Trivago/Vista/GuardarHotel.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trivago.Modelo;
+using Trivago.VistasModelo;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Trivago.Vista
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class GuardarHotel : ContentPage
+	{
+		public GuardarHotel ()
+		{
+			InitializeComponent ();
+		}
+
+        private void btnGuardar_Clicked(object sender, EventArgs e)
+        {
+			guardarHotel();
+        }
+
+		private async Task guardarHotel()
+		{
+			MHotel mHotel = new MHotel();
+			MVHotel metodo = new MVHotel();
+
+			mHotel.Nombre = nombre.Text;
+			mHotel.Contacto = contacto.Text;
+			mHotel.Direccion = direccion.Text;
+
+			await metodo.InsertarHotel(mHotel);
+			await DisplayAlert("alerta", "Hotel guardado con exito", "OK");
+		}
+    }
+}
