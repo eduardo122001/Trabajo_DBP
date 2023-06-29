88b5b06acbb2490aef08de4d6a666b6f118f7154
eduardo122001 <eduardo.cruz@ucsp.edu.pe>
2023-06-29 10:40:12 -0500

    form

---
+++ Trivago Xamarin/form/Trivago/Trivago/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Trivago.Vistas;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Trivago
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new Reservacion_form());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
