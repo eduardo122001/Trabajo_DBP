e55b095e59746b81a99ab93e165bc40c33181407
RoNyOsCaRxd <rony.choquecondo@ucsp.edu.pe>
2023-06-29 11:49:09 -0500

    loginxd

---
+++ Trivago Xamarin/trivago/trivago/View/botonView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace trivago.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class botonView : ContentPage
+    {
+        public botonView()
+        {
+            InitializeComponent();
+        }
+        private async void iniciarSesion_Clicked(object sender, EventArgs e)
+        {
+            await Application.Current.MainPage.Navigation.PushModalAsync(new iniciarsesionView());
+        }
+
+        private async void registrar_Clicked(object sender, EventArgs e)
+        {
+            await Application.Current.MainPage.Navigation.PushModalAsync(new registrarseView());
+        }
+    }
+}
---
+++ Trivago Xamarin/trivago/trivago/View/registrarseView.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using trivago.Modelo;
+using trivago.VistaModelo;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace trivago.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class registrarseView : ContentPage
+    {
+        public registrarseView()
+        {
+            InitializeComponent();
+        }
+       
+        private async void reregresar_Clicked(object sender, EventArgs e)
+        {
+            await Application.Current.MainPage.Navigation.PopModalAsync();
+        }
+
+        private async void btn_guardar_Clicked(object sender, EventArgs e)
+        {
+            await guardarcontacto();
+        }
+
+        private async Task guardarcontacto()
+        {
+            MUusuario mUusuario= new MUusuario();
+            MVUusuario metodo= new MVUusuario();
+
+            
+
+            mUusuario.nombre = x_nombre.Text;
+            mUusuario.apellido = x_apellido.Text;
+            mUusuario.correo= x_correo.Text;
+            mUusuario.contraseña = x_contraseña.Text;
+
+            await metodo.InsertarUsuario(mUusuario);
+            await DisplayAlert("cargando", "guardando", "acepta :)");
+
+            // iniciarsesionView iniciarsesion = new iniciarsesionView(x_correo.Text, x_contraseña.Text);
+            //await Application.Current.MainPage.Navigation.PushModalAsync(iniciarsesion);
+
+        }
+    }
+}
